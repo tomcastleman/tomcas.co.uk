fd76859f00dee1021de78675329ac33adb9fb3cd
Tom Castleman <tomcastleman@users.noreply.github.com>
2021-03-12 17:54:44 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'build' => [
+        'destination' => 'docs',
+    ],
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'TOMCAS Consulting',
+    'description' => 'TOMCAS Consulting',
+    'collections' => [],
+];
